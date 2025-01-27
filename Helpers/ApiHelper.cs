99497bcff1555be86b3a72ea6fa48fac0f00f968
Bismarck Cerda A. <bisgael_7@hotmail.com>
2025-01-27 13:18:25 -0600

    Añadir método de extensión ToActionResult para CustomResult en ApiHelpers
    - Se ha añadido el método de extensión `ToActionResult` en la clase `ApiHelpers` para convertir un objeto `CustomResult` en un `IActionResult`.
    - El método permite mapear de forma sencilla un `CustomResult` a un `ObjectResult` con los valores de éxito, mensaje y contenido, así como el código de estado HTTP correspondiente.
    - Esto facilita el manejo y la conversión de resultados personalizados a respuestas HTTP en los controladores de la API.

---
+++ Helpers/ApiHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DailyHelpers
+{
+    public static class ApiHelpers
+    {
+
+        /// <summary>
+        /// Convierte un objeto <see cref="CustomResult"/> en un resultado de acción HTTP (<see cref="IActionResult"/>).
+        /// </summary>
+        /// <param name="customResult">El objeto <see cref="CustomResult"/> que contiene el resultado de la operación.</param>
+        /// <returns>Un <see cref="IActionResult"/> que representa el resultado de la operación, con el mensaje, el contenido y el código de estado HTTP correspondientes.</returns>
+        /// <remarks>
+        /// Este método permite mapear de manera sencilla un <see cref="CustomResult"/> a un <see cref="ObjectResult"/> que se puede utilizar 
+        /// en los controladores de una API para devolver respuestas personalizadas con un código de estado HTTP.
+        /// </remarks>
+        public static IActionResult ToActionResult(this CustomResult customResult)
+        {
+            return new ObjectResult(new
+            {
+                success = customResult.Succeeded,
+                message = customResult.Message,
+                content = customResult.Content
+            })
+            {
+                StatusCode = (int)customResult.HttpStatusCode
+            };
+        }
+    }
+}
